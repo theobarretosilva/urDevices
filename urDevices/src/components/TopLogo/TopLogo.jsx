1632281091410070ef36c12cdd5f1b3d3e4c94fb
Théo Barreto Silva <barretotheo25@gmail.com>
2022-10-05 19:13:19 -0300

    upload

--- urDevices/src/components/TopLogo/TopLogo.jsx
+++ urDevices/src/components/TopLogo/TopLogo.jsx
@@ -3,7 +3,7 @@ import { DivLogoStyled, Logo1Styled, NomeLogoStyled } from "./TopLogo.styles"
 export const TopLogo = () => {
     return(
         <DivLogoStyled>
-            <Logo1Styled src="../../src/assets/imgs/logo1.png" alt="Logo Connect Lab"/>
+            <Logo1Styled src="https://github.com/theobarretosilva/urDevices/blob/main/urDevices/src/assets/imgs/logo1.png?raw=true" alt="Logo Connect Lab"/>
             <NomeLogoStyled>Connect Lab</NomeLogoStyled>
         </DivLogoStyled>
     )
